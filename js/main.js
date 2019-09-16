cdc96a6d3ac9d55182c75c42fa7c722da1df9135
geoSkogen <borninthecaul@gmail.com>
2019-09-15 19:41:16 -0700

    adds labeled rows & columns

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 
 var Schema = require('./schema.js')
 var DeepNest = require('./deep_nest.js')
-var filename = process.argv[2]
-var this_schema = new Schema(filename, '../records')
+//var filename = process.argv[2]
+var this_schema = new Schema('col-data', '../records')
+/*
 var this_nester = new DeepNest(this_schema.data.index)
 var mkdir_export_str = this_nester.get_mkdir_lines('content-dir')
 var echo_export_str = this_nester.get_nested_index_echoes(
@@ -16,3 +17,4 @@ this_nester.export_batch_commands(echo_export_str,'indexing','batch_files')
 
 //this_schema.export_csv( export_str, 'myexport', '../exports' )
 header("Location: http://lotuseaters/saturn_3/", true, 301);
+*/
--- js/schema.js
+++ js/schema.js
@@ -31,6 +31,46 @@ class Schema {
     return { assoc: export_arr, index: data_arr }
   }
 
+  get_labeled_columns( data_arr ) {
+    var keys =  []
+    var result = []
+    for (let row_index = 0; row_index < data_arr.length; row_index++) {
+      for (let i = 0; i < data_arr[row_index].length; i++) {
+        if (row_index === 0) {
+          result[data_arr[row_index][i]] = []
+          keys.push(data_arr[row_index])
+        } else {
+          if (data_arr[row_index][i]) {
+            result[keys[i]].push(data_arr[row_index][i])
+          }
+        }
+      }
+    }
+    return result
+  }
+
+  get_labeled_rows( data_arr ) {
+    var key = ''
+    var valid_data = []
+    var result = []
+    for (let row_index = 0; row_index < data_arr.length; row_index++) {
+      var valid_data = []
+      for (let i = 0; i < data_arr[row_index].length; i++) {
+        if (i === 0) {
+          key = data_arr[row_index][i]
+        } else {
+          if (data_arr[row_index][i]) {
+            valid_data.push(data_arr[row_index][i])
+          }
+          if (i === data_arr[row_index].length-1) {
+            result[key] = valid_data
+          }
+        }
+      }
+    }
+    return result
+  }
+
   lookup_val( keyname, index ) {
     var result = false
     var key_index = -1
