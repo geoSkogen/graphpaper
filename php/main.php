b5679d0b7aa6d699f853c2ff95fd9b5b5184fcb9
geoSkogen <borninthecaul@gmail.com>
2019-09-26 22:42:47 -0700

    used to make edits to geolocation file

--- php/main.php
+++ php/main.php
@@ -4,10 +4,80 @@ require 'schema.php';
 require 'deep_nest.php';
 require 'content_monster.php';
 
-$this_schema = new Schema('geo2-csvnest-row', '../records');
+$new_schema = [];
+$new_row = [];
+$this_schema = new Schema('criteria-ids-ltd', '../records');
+$zip_schema = new Schema('zipcodes', '../records');
+$zip_lookup = array('zips'=>[], 'names'=>[]);
+$index = '';
+$found_name = '';
+$zip_not_found = [];
+
+foreach($zip_schema->data_index as $zip_row) {
+  $zip_lookup['zips'][] = $zip_row[0];
+  $zip_lookup['names'][] = ucwords(strtolower($zip_row[1]));
+}
+foreach ($this_schema->data_index as $data_row) {
+  $region = '';
+  $new_row = [];
+  if (is_numeric($data_row[1])) {
+    if ( array_search($data_row[1],$zip_lookup['zips']) ||
+      array_search($data_row[1],$zip_lookup['zips']) === 0) {
+      $index = array_search($data_row[1],$zip_lookup['zips']);
+      $found_name =  $zip_lookup['names'][$index];
+      $new_row = array(
+        $data_row[0],
+        $found_name,
+        $data_row[2],
+        $data_row[3],
+        $data_row[4],
+        $data_row[5],
+        $data_row[6],
+        $data_row[7],
+        $data_row[8],
+      );
+      array_push($new_schema,$new_row);
+    } else {
+      $zip_not_found[] = $data_row[1];
+    }
+  } else {
+    array_push($new_schema,$data_row);
+  }
+
+  /*
+  switch($data_row[3]) {
+    case 'Baltimore North Pest':
+    case 'Arundel / Biddeford Atlantic Pest':
+    case 'Buffalo Pest':
+    case 'Reading Pest':
+    case 'Pittsburgh Pest':
+      $region = 'Northeast';
+      break;
+    case 'Virginia':
+      $region = 'Southeast';
+      break;
+  }
+  $new_row = array(
+    $data_row[0],
+    $data_row[1],
+    $data_row[2],
+    $data_row[3],
+    $data_row[4],
+    $region,
+    $data_row[5]
+  );
+  */
+}
+error_log('not found :' . strval(count($zip_not_found)));
+error_log(var_dump($zip_not_found));
+$str = Schema::make_export_str($new_schema);
+Schema::export_csv($str,'criteria-ids-ltd-nozip','exports');
+
+/*
+$this_schema = new Schema('geo3-csvnest-row', '../records');
 $arr = $this_schema->data_index;
 $i = 0;
-$base_url = "'https://ehrlich-ne.pestcontroloffers.com/home/";
+$base_url = "'https://wordpress_1/page/";
 $buffalo = [];
 $pittsburgh = [];
 $reading = [];
@@ -51,7 +121,7 @@ foreach ($keys as $key) {
   }
   file_put_contents("../" . "exports" . "/" . $key . ".txt" , $col);
 }
-
+*/
 
 /*
 foreach($this_schema->data_index as $row) {
