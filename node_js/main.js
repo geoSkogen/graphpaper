7463689968efb70c013d7f176111df5ae4848004
Joseph Scoggins <borninthecaul@gmail.com>
2020-01-09 11:55:18 -0800

    adds FAQ schema script

--- node_js/main.js
+++ node_js/main.js
@@ -1,6 +1,10 @@
 'use strict'
 
 var Schema = require('./schema.js')
+var this_schema = new Schema('faq-you', '../records')
+var new_array = []
+var new str = ''
+/*
 var DeepNest = require('./deep_nest.js')
 var CMonster = require('./content_monster.js')
 
@@ -18,3 +22,49 @@ var mkdir_export_str = this_nester.get_mkdir_lines(c_monster._DIR)
 var echo_export_str = this_nester.get_nested_index_echoes(c_monster)
 this_nester.export_batch_commands(mkdir_export_str,'nesting','batch_files')
 this_nester.export_batch_commands(echo_export_str,'indexing','batch_files')
+*/
+var jsonData = {
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "mainEntity": []
+}
+var faqObj = {
+  "@type": "Question",
+  "name": "",
+  "acceptedAnswer": {
+    "@type": "Answer",
+    "text": ""
+  }
+}
+var this_faq = faqObj
+var even = true
+var even_even = false
+this_schema.data.index.forEach(
+   function (e) {
+    if (even) {
+      if (even_even) {
+        e.forEach( function (ee) {
+          this_faq.acceptedAnswer.text += ee
+        })
+        new_array.push(this_faq)
+        this_faq = {
+          "@type": "Question",
+          "name": "",
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": ""
+          }
+        }
+      } else {
+        e.forEach( function (eee) {
+          this_faq.name += eee
+        })
+      }
+      even_even = !even_even
+    }
+  even = !even
+  }
+)
+jsonData.mainEntity = new_array
+new_str = JSON.stringify(jsonData)
+this_schema.export_csv(new_data, 'myjson', 'exports')
