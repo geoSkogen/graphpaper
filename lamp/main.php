51c826c7130efe631ae679cda5d0e68be84d7654
Joseph Scoggins <borninthecaul@gmail.com>
2020-12-26 14:09:36 -0800

    adds geo rel routine

--- lamp/main.php
+++ lamp/main.php
@@ -4,18 +4,37 @@ require 'schema.php';
 //require 'sitemap_monster.php';
 //require 'deep_nest.php';
 //require 'content_monster.php';
-
+/*
 $new_schema = [];
 $new_row = [];
-
-$my_schema = new Schema('test','../records');
+*/
+$my_schema = new Schema('geo5','../records');
 $my_table = $my_schema->data_index;
-error_log(print_r($my_table));
+//export tables
+$locale_ids = [];
+$criteria_ids = [];
+//metadata
+$locale_names = array();
+$locale_index = 0;
+//sorting routine
+foreach($my_table as $row) {
+  if (empty($locale_names[$row[3]])) {
+    $locale_names[$row[3]] = $locale_index;
+    $locale_ids[$locale_index] = [ $row[3],$row[4],$row[5],$row[6],$row[7] ];
+    $locale_index++;
+  }
 
-error_log('schema size:');
-error_log(strval(count($new_schema)));
-
-$my_str = Schema::make_export_str($new_schema);
-Schema::export_csv($my_str,'my-file','../exports');
+  $criteria_ids[] = [ $row[0],$row[1],$row[2],$locale_names[$row[3]] ];
+}
+//report
+error_log('schema size crit ids:');
+error_log(strval(count($criteria_ids)));
+error_log('schema size locale ids:');
+error_log(strval(count($locale_ids)));
+//export
+$locale_str = Schema::make_export_str($locale_ids);
+Schema::export_csv($locale_str,'geo5-locales','../exports');
+$criteria_str = Schema::make_export_str($criteria_ids);
+Schema::export_csv($criteria_str,'geo5-ids','../exports');
 
 ?>
